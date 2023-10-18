e491588300e0341b75cceb504f056aee1116ed61
TheLoneWnderer <evilangel3012@gmail.com>
2023-10-18 11:42:27 +0200

    try n6

--- vulnerable-flask-app.py
+++ vulnerable-flask-app.py
@@ -233,3 +233,4 @@ if __name__ == '__main__':
 '''comment n4 after making a change in line 46 in the CI workflow'''
 '''comment n5 and hopefully the last'''
 '''i believe this is the last comment'''
+'''okay this is the last (I hope)'''
